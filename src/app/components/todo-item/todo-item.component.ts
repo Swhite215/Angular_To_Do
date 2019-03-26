75d095d55dd75daee1615c4e3c7d313ac3c2d67f
Spencer White <spencer.white@carat.com>
2019-03-26 13:26:14 -0400

    updated onToggle method to update api and todo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import Todo from "../../models/Todo";
+import { TodoService } from "../../services/todo.service";
+
 @Component({
   selector: "app-todo-item",
   templateUrl: "./todo-item.component.html",
@@ -8,7 +10,7 @@ import Todo from "../../models/Todo";
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {}
 
@@ -23,7 +25,11 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    //Toggle In UI
     todo.completed = !todo.completed;
+
+    //Toggle On Server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
