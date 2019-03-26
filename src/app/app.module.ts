9e807a6d6fea84ef4a4697c9894e3366e24d748a
Spencer White <spencer.white@carat.com>
2019-03-26 14:21:04 -0400

    added about component to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TodosComponent } from "./components/todos/todos.component";
 import { TodoItemComponent } from "./components/todo-item/todo-item.component";
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { AddTodoComponent } from "./components/add-todo/add-todo.component";
+import { AboutComponent } from './components/pages/about/about.component';
 
 //Entry Point to Angular - Meeting Place for Components
 //Declarations - Components
@@ -20,7 +21,8 @@ import { AddTodoComponent } from "./components/add-todo/add-todo.component";
     TodosComponent,
     TodoItemComponent,
     HeaderComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
