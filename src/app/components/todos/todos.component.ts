cc87d96dd81711ea28c4830a068c810af95f38e0
Spencer White <spencer.white@carat.com>
2019-03-26 11:32:23 -0400

    new todos component and supplementary files

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
+})
+export class TodosComponent implements OnInit {
+  //Constructor used for importing services
+  constructor() {}
+
+  //Similar to Component Did Mount
+  ngOnInit() {}
+}
