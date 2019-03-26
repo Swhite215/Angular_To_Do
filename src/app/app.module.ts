fd8af1df22a5cadccaf0a922153b199f8c86b89d
Spencer White <spencer.white@carat.com>
2019-03-26 11:25:55 -0400

    notes on the Ng entry point module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
+//Entry Point to Angular - Meeting Place for Components
+//Declarations - Components
+//Imports - Additional Modules
+//Providers - Services
+//Bootstrap - Root Component Inserted Into Index.html Page
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
