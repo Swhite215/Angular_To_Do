dc2cd9dfc92d57cfacfdd7a8f8cfed4bc69b8cc4
Spencer White <spencer.white@carat.com>
2019-03-26 13:25:50 -0400

    added toggleCompleted method and http headers

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,11 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import Todo from "../models/Todo";
 import { Observable } from "rxjs";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
 @Injectable({
   providedIn: "root"
 })
@@ -12,7 +17,14 @@ export class TodoService {
   todosUrl: string = "https://jsonplaceholder.typicode.com/todos";
   query: string = "?_limit=10";
 
+  //GET - Todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl + this.query);
+  }
+
+  //PUT - Toggle Todo
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
