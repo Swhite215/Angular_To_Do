8d9c3b9afcedca7bb44d9e1edec871968b4cb1c7
Spencer White <spencer.white@carat.com>
2019-03-26 11:25:39 -0400

    notes and example method in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+//Component File
+//@Component Decorator - Meta-Data: Selector, templateUrl, styleUrls
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+
+//Component = Class w/Properties
 export class AppComponent {
-  title = 'angular-todo';
+  title: string = "Spencer's To-Do Application";
+
+  constructor() {
+    this.changeTitle("Hello World!");
+  }
+
+  changeTitle(name: string) {
+    this.title = name;
+  }
 }
