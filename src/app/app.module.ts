5ed6c7bde2ee955fa0613997a597108d85f8cca1
Spencer White <spencer.white@carat.com>
2019-03-26 14:07:30 -0400

    added FormsModule and AddTodo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TodosComponent } from "./components/todos/todos.component";
 import { TodoItemComponent } from "./components/todo-item/todo-item.component";
-import { HeaderComponent } from './components/layout/header/header.component';
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { AddTodoComponent } from "./components/add-todo/add-todo.component";
 
 //Entry Point to Angular - Meeting Place for Components
 //Declarations - Components
@@ -13,8 +15,14 @@ import { HeaderComponent } from './components/layout/header/header.component';
 //Providers - Services
 //Bootstrap - Root Component Inserted Into Index.html Page
 @NgModule({
-  declarations: [AppComponent, TodosComponent, TodoItemComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
